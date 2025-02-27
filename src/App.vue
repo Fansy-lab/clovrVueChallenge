c9322206f2496aa2de1241fd14abc54af2d9f7e5
fansy <ffaannssyy@gmail.com>
2025-02-27 16:19:02 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import RandomComponent from "./components/RandomComponent.vue";
-import OK from "./components/OK.vue";
 </script>
 
 <template>
   <RandomComponent />
-  <!-- <OK></OK> -->
 </template>
